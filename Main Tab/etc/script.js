4ce47c18783b8f1d602f200d9bb69d3cb2d2ef0e
Weird_Kusou <amanwhohave@gmail.com>
2021-02-12 15:22:00 +0700

    Update script.js

--- Main Tab/etc/script.js
+++ Main Tab/etc/script.js
@@ -18,3 +18,74 @@ function Allert(){
 		}
 	})
 };
+
+		var myVar;
+	function Loading() {
+  		myVar = setTimeout(showPage, 1000);
+	}
+
+	function showPage() {
+  		document.getElementById("loader").style.display = "none";
+  		document.getElementById("myDiv").style.display = "block";
+	}
+	
+	function DarkMode() {
+		var element = document.body;
+   		var wosh = document.getElementById("wosh");
+			wosh.volume= 0.5;
+			wosh.play(); 
+   		element.classList.toggle("darkmode");
+	}
+	function overview(){
+		var element = document.body;
+		element.classList.toggle("overview");
+	}
+	function Hamburger() {
+   		var element = document.body;
+   		element.classList.toggle("ham");
+	}
+	function task(){
+		var element = document.body;
+		element.classList.toggle("tugas");
+	}
+	function TwoNdTask(){
+		var element = document.body;
+		element.classList.toggle("TwoNdTugas");
+	}
+	function ExtendTask(){
+		var element = document.body;
+		element.classList.toggle("ExtendTugas");
+	}
+	function MaxTask(){
+		var element = document.body;
+		element.classList.toggle("MaxTugas");
+	}
+	function ActivatedTugas(){
+		var element = document.body;
+		element.classList.toggle("AcTugas");
+		
+	}
+	function avg(){
+		window.scrollTo(0,document.body.scrollHeight);
+	}
+	
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navigation");
+var navbar2 = document.getElementById("navigation1");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+	  navbar.classList.add("sticky")
+	  navbar2.classList.add("navUsed")
+  } else {
+    navbar.classList.remove("sticky")
+	navbar2.classList.remove("navUsed");
+  }
+}
