34d296fa6c080f1f41d095f0ba0200c2825e7704
Weird_Kusou <amanwhohave@gmail.com>
2021-02-12 14:52:16 +0700

    Update script.js

--- etc/script.js
+++ etc/script.js
@@ -175,3 +175,25 @@ function mouseOverIco4(){
 function mouseOutIco4(){
 	ico4.classList.remove("FIconHover");
 }
+
+//Get the button:
+var Top1 = document.getElementById("TopButton");
+Top = document.getElementById("TopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	  Top1.classList.remove("TopUsed");
+  }
+	else {
+		Top1.classList.add("TopUsed");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function GoTOTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
