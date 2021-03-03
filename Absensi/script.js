1acd94b898502094e2b2421765858413bbceb197
UTA <78462249+uta17@users.noreply.github.com>
2021-03-04 00:35:58 +0700

    Add files via upload

---
+++ Absensi/script.js
@@ -0,0 +1,21 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+
+function Allert(){
+	Swal.fire({
+        title: 'Informasi',
+        icon: 'info',
+        text: 'Pastikan Meminta Izin Cuti Sebelum Hari H',
+	    showDenyButton: true,
+        confirmButtonText: "Ya"
+	}).then((Answer) => {
+        if ( Answer.isConfirmed ){
+            location.href="Cuti.html";
+        }
+    })
+};
