6ce3d2b0c86a621e18f151bf1677574e5e700c8f
Weird_Kusou <amanwhohave@gmail.com>
2021-03-05 09:00:33 +0700

    Update and rename pengumuman.html to pengumuman.php

--- Pengumuman/pengumuman.php
+++ Pengumuman/pengumuman.php
@@ -1,3 +1,41 @@
+<?php
+require_once("../config.php");
+
+if(isset($_POST['PROFIL'])){
+	$nik = $_GET['nik'];
+		
+		$sql = mysqli_query($konek, "SELECT * FROM login WHERE NIK='$nik'");
+		
+		if (mysqli_num_rows($sql) != 0){
+			$A = "SELECT * FROM login WHERE NIK='$nik';";
+			$result = mysqli_query($konek, $A);
+			$check = mysqli_num_rows($result);
+				
+			if ($check > 0){
+				while ($row = mysqli_fetch_assoc($result)){
+					$nama = $row['Nama'];
+					$tgl = $row['Tanggal_Lahir'];
+					$bln = $row['Bulan_Lahir'];
+					$thn = $row['Tahun_Lahir'];
+					$jabatan = $row['Jabatan'];
+					$tel = $row['No_Telp'];
+					$email = $row['Email'];
+					
+					//date in mm/dd/yyyy format; or it can be in other formats as well
+  					$birthDate = "$bln/$tgl/$thn";
+  					//explode the date to get month, day and year
+  					$birthDate = explode("/", $birthDate);
+  					//get age from date or birthdate
+  					$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    				? ((date("Y") - $birthDate[2]) - 1)
+    				: (date("Y") - $birthDate[2]));
+					
+					header("Location: ../Main Tab/etc/Main.php?nama=$nama && umur=$age && jabatan=$jabatan && nik=$nik && tel=$tel && email=$email");
+				}
+			}
+		}
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -218,7 +256,9 @@
 		<span>Pengumuman</span>
 		<span>Officia</span>
 	</div>
-			<p class="judul2"><a href="../Main Tab/etc/Main.php">Profil</p></a>
+		<form id="form1" name="form1" method="post" action="">
+			<p class="judul2"><button style="background-color: transparent;border: none;" type="submit" name="PROFIL" id="PROFIL" value="profil"><a>Profil</a></button></p>
+		</form>
 	</div>
 <br>
 <br>
