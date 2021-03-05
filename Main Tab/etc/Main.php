67b8c37b8be96a565b3377a4235d4c8d6513021b
Weird_Kusou <amanwhohave@gmail.com>
2021-03-05 08:59:57 +0700

    Update Main.php

--- Main Tab/etc/Main.php
+++ Main Tab/etc/Main.php
@@ -1,3 +1,76 @@
+<?php
+require_once("../../config.php");
+
+if(isset($_POST['PENGUMUMAN'])){
+	$nik = $_GET['nik'];
+		
+		$sql = mysqli_query($konek, "SELECT * FROM login WHERE NIK='$nik'");
+		
+		if (mysqli_num_rows($sql) != 0){
+			$A = "SELECT * FROM login WHERE NIK='$nik';";
+			$result = mysqli_query($konek, $A);
+			$check = mysqli_num_rows($result);
+				
+			if ($check > 0){
+				while ($row = mysqli_fetch_assoc($result)){
+					$nama = $row['Nama'];
+					$tgl = $row['Tanggal_Lahir'];
+					$bln = $row['Bulan_Lahir'];
+					$thn = $row['Tahun_Lahir'];
+					$jabatan = $row['Jabatan'];
+					$tel = $row['No_Telp'];
+					$email = $row['Email'];
+					
+					//date in mm/dd/yyyy format; or it can be in other formats as well
+  					$birthDate = "$bln/$tgl/$thn";
+  					//explode the date to get month, day and year
+  					$birthDate = explode("/", $birthDate);
+  					//get age from date or birthdate
+  					$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    				? ((date("Y") - $birthDate[2]) - 1)
+    				: (date("Y") - $birthDate[2]));
+					
+					header("Location: ../../Pengumuman/pengumuman.php?nama=$nama && umur=$age && jabatan=$jabatan && nik=$nik && tel=$tel && email=$email");
+				}
+			}
+		}
+	}
+
+if(isset($_POST['ABSEN'])){
+	$nik = $_GET['nik'];
+		
+		$sql = mysqli_query($konek, "SELECT * FROM login WHERE NIK='$nik'");
+		
+		if (mysqli_num_rows($sql) != 0){
+			$A = "SELECT * FROM login WHERE NIK='$nik';";
+			$result = mysqli_query($konek, $A);
+			$check = mysqli_num_rows($result);
+				
+			if ($check > 0){
+				while ($row = mysqli_fetch_assoc($result)){
+					$nama = $row['Nama'];
+					$tgl = $row['Tanggal_Lahir'];
+					$bln = $row['Bulan_Lahir'];
+					$thn = $row['Tahun_Lahir'];
+					$jabatan = $row['Jabatan'];
+					$tel = $row['No_Telp'];
+					$email = $row['Email'];
+					
+					//date in mm/dd/yyyy format; or it can be in other formats as well
+  					$birthDate = "$bln/$tgl/$thn";
+  					//explode the date to get month, day and year
+  					$birthDate = explode("/", $birthDate);
+  					//get age from date or birthdate
+  					$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    				? ((date("Y") - $birthDate[2]) - 1)
+    				: (date("Y") - $birthDate[2]));
+					
+					header("Location: ../../Absensi/Home.php?nama=$nama && umur=$age && jabatan=$jabatan && nik=$nik && tel=$tel && email=$email");
+				}
+			}
+		}
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -473,7 +546,7 @@
 		
 		<nav id="navigation1" class="nav1">
 </nav>
-		
+		<form id="form1" name="form1" method="post" action="">
 <nav id="navigation">
 		<div class="menu-toggle">
 		<input type="checkbox" onClick="Hamburger()">
@@ -489,12 +562,13 @@
 
         <ul>
             <div class="wow fadeInLeft"><li><a><u>Profile</u></a></li></div>
-            <div class="wow fadeInLeft"><li><a href="../../Absensi/Home.html">Absen</a></li></div>
-			<div class="wow fadeInLeft"><li><a href="../../Pengumuman/pengumuman.html">Pengumuman</a></li></div>
+            <div class="wow fadeInLeft"><li><button style="background-color: transparent;border: none;" type="submit" name="ABSEN" id="ABSEN" value="absen"><a>Absen</a></li></div>
+			<div class="wow fadeInLeft"><li><button style="background-color: transparent;border: none;" type="submit" name="PENGUMUMAN" id="PENGUMUMAN" value="pengumuman"><a>Pengumuman</a></button></li></div>
             <div class="wow fadeInLeft"><li><a onclick="Allert()">Log-out</a></li></div>
 			<div class="wow fadeInLeft"><li><button onclick="DarkMode()" class="dark"><img src="https://static.thenounproject.com/png/1664849-200.png" alt="" height="23px"></button></li></div>
         </ul>
 </nav>
+				</form>
 	</div>
 	<div class="bio">
 		<table class="center">	
