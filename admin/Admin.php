3faa11f9b8e7b000e2126fcb2be4efeb35fdda8b
Weird_Kusou <amanwhohave@gmail.com>
2021-03-09 18:41:01 +0700

    Update Admin.php

--- admin/Admin.php
+++ admin/Admin.php
@@ -1,5 +1,30 @@
 <?php
 require_once("../config.php");
+$kantor = $_GET['kantor'];
+$nik = $_GET['nik'];
+$pw = $_GET['password'];
+
+if(isset($_POST['SET_ABSEN'])){
+		
+		$sql = mysqli_query($konek, "SELECT * FROM data_perusahaan WHERE NIK_Admin='$nik' AND Password='$pw' AND Nama_Perusahaan='$kantor'");
+		
+		if (mysqli_num_rows($sql) != 0){
+			$A = "SELECT * FROM data_perusahaan WHERE NIK_Admin='$nik' AND Password='$pw' AND Nama_Perusahaan='$kantor';";
+			$result = mysqli_query($konek, $A);
+			$check = mysqli_num_rows($result);
+				
+			if ($check > 0){
+				while ($row = mysqli_fetch_assoc($result)){
+					$datang_min = $row['Absen_datang_min'];
+					$datang_max = $row['Absen_datang_max'];
+					$pulang_min = $row['Absen_pulang_min'];
+					$pulang_max = $row['Absen_pulang_max'];
+					
+					header("Location: Setting-Absen.php?kantor=$kantor && nik=$nik && password=$pw && datang_min=$datang_min && datang_max=$datang_max && pulang_max=$pulang_max && pulang_min=$pulang_min");
+				}
+			}
+		}
+	}
 
 ?>
 <!DOCTYPE html>
@@ -122,14 +147,16 @@ require_once("../config.php");
             </div>
        <div class="Banner-handap">
             <div class="o">Officia</div>
+		   <form id="form1" name="form1" method="post" action="">
             <ul class="inline">
-                <li><a href="Setting-Absen.html" class="absen">Setting Absen<i class="logo fas fa-calendar-check"></i></a></li>
+                <li><button style="background-color: transparent;border: none;" type="submit" name="SET_ABSEN" id="SET_ABSEN" value="set_absen"><a class="absen">Setting Absen<i class="logo fas fa-calendar-check"></i></a></button></li>
                 <li><a href="+Pengumuman.html" class="cuti">Izin Cuti<i class="logo fas fa-calendar-minus"></i></a></li>
                 <li><a href="" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
                 <li><a href="" class="karyawan">Karyawan<i class="logo fas fa-id-card"></i></a></li>
                 <li><a href="" class="list-karyawan">List Karyawan<i class="logo fas fa-tasks"></i></a></li>
                 <li><a href="" class="tugas">Tugas<i class="logo fas fa-briefcase"></i></a></li>
             </ul>
+		   </form>
         </div>     
     </body>
 </html>
