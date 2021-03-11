0c12bfb7fb7cbb9afebf6b7defcc2ec47eb22d05
Weird_Kusou <amanwhohave@gmail.com>
2021-03-11 15:27:22 +0700

    Update unused.php

--- unused.php
+++ unused.php
@@ -14,7 +14,6 @@ session_start();
 		session_destroy();
 		session_write_close();
 		setcookie(session_name(),'',0,'/');
-		session_regenerate_id(true);
 		
 		header("Location: index.html");
 	exit();
