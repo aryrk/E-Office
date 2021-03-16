de6070b465338e49da5ddd409be873cb5678c580
Weird_Kusou <amanwhohave@gmail.com>
2021-03-16 19:01:42 +0700

    Update script.js

--- admin/etc/script.js
+++ admin/etc/script.js
@@ -8,5 +8,12 @@ function autosize(){
     // for box-sizing other than "content-box" use:
     // el.style.cssText = '-moz-box-sizing:content-box';
     el.style.cssText = 'height:' + el.scrollHeight + 'px';
+	this.value = this.value + "\n";
   },0);
+}
+document.getElementsByClassName("toggler-navbar")[0].addEventListener("click", toogleClass);
+
+function toogleClass() {
+    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open')
+    document.getElementsByClassName("sidebar")[0].classList.toggle('open')
 }
