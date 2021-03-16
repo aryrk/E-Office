ce69a152d006723c01d498c3cef00e314cdd3b50
Weird_Kusou <amanwhohave@gmail.com>
2021-03-16 22:19:16 +0700

    Add files via upload

---
+++ Main Tab/etc/upload/index.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+//mencegah user masuk bila mereka belum melakukan login
+if (!isset($_SESSION['LOGIN'])){
+	header("Location: ../../Login/login1.php");
+	exit ();
+}
+?>
+<html>  
+    <head>  
+        <title>Upload Image</title>  
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel = "icon" href ="../../../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
+		<script src="jquery.min.js"></script>  
+		<script src="bootstrap.min.js"></script>
+		<script src="croppie.js"></script>
+		<link rel="stylesheet" href="bootstrap.min.css" />
+		<link rel="stylesheet" href="croppie.css" />
+		
+		<style>
+		img[alt*="000webhost"],
+		img[alt*="000webhost"][style],
+		img[src*="000webhost"],
+		img[src*="000webhost"][style],
+		body > div:nth-last-of-type(1)[style]{
+			opacity: 0 !important;
+			pointer-events:none !important;
+			width: 0px !important;
+			height: 0px !important;
+			visibility:hidden !important;
+			display:none !important;
+		}
+		</style>
+    </head>  
+    <body>  
+        <div class="container">
+          <br />
+      <h3 align="center">Pilih dan Crop Foto</h3>
+      <br />
+      <br />
+			<div class="panel panel-default">
+  				<div class="panel-heading">Select Profile Image. <a href="../Main.php">Back</a></div>
+  				<div class="panel-body" align="center">
+  					<input type="file" name="upload_image" id="upload_image" />
+  					<br/>
+  					<div id="uploaded_image"></div>
+  				</div>
+  			</div>
+  		</div>
+    </body>  
+</html>
+
+<div id="uploadimageModal" class="modal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+      		<div class="modal-header">
+        		<button type="button" class="close" data-dismiss="modal">&times;</button>
+        		<h4 class="modal-title">Upload & Crop Image</h4>
+      		</div>
+      		<div class="modal-body" style="height: 500px;">
+        		<div class="row">
+  					<div class="col-md-8 text-center">
+						<button class="btn btn-success crop_image">Crop & Upload Image</button>
+						  <div id="image_demo" style="width:350px; margin-top:30px;"></div>
+  					</div>
+				</div>
+      		</div>
+      		<div class="modal-footer">
+        		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      		</div>
+    	</div>
+    </div>
+</div>
+
+<script>  
+$(document).ready(function(){
+
+	$image_crop = $('#image_demo').croppie({
+    enableExif: true,
+    viewport: {
+      width:200,
+      height:200,
+      type:'square' //circle
+    },
+    boundary:{
+      width:300,
+      height:300
+    }
+  });
+
+  $('#upload_image').on('change', function(){
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      $image_crop.croppie('bind', {
+        url: event.target.result
+      }).then(function(){
+        console.log('jQuery bind complete');
+      });
+    }
+    reader.readAsDataURL(this.files[0]);
+    $('#uploadimageModal').modal('show');
+  });
+
+  $('.crop_image').click(function(event){
+    $image_crop.croppie('result', {
+      type: 'canvas',
+      size: 'viewport'
+    }).then(function(response){
+      $.ajax({
+        url:"upload.php",
+        type: "POST",
+        data:{"image": response},
+        success:function(data)
+        {
+          $('#uploadimageModal').modal('hide');
+          $('#uploaded_image').html(data);
+        }
+      });
+    })
+  });
+
+});  
+</script>
---
+++ Main Tab/etc/upload/upload.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+//mencegah user masuk bila mereka belum melakukan login
+if (!isset($_SESSION['LOGIN'])){
+	header("Location: ../../../Login/login1.php");
+	exit ();
+}
+
+
+require_once("../../../config.php");
+//Session akan membuat link terlihat polos dan membuat website lebih teroptimisasi dibanding sebelumnya
+$nik = $_SESSION['nik'];
+$kantor = $_SESSION['kantor'];
+$pass = $_SESSION['password'];
+
+if(isset($_POST["image"]))
+{
+	$data = $_POST["image"];
+	$image_array_1 = explode(";", $data);
+	$image_array_2 = explode(",", $image_array_1[1]);
+	$data = base64_decode($image_array_2[1]);
+
+	$imageName = $nik . $kantor . time() . '.png';
+
+	$sql = mysqli_query($konek, "SELECT pp_name FROM login WHERE NIK='$nik' AND Nama_Perusahaan='$kantor'");
+		if (mysqli_num_rows($sql) != 0){
+			$row = mysqli_fetch_assoc($sql);
+			$pp_name = $row['pp_name'];
+			
+			if ($pp_name != "default.png"){
+				unlink('image/'.$pp_name);
+				file_put_contents('image/'.$imageName, $data);
+			}
+			else {
+				file_put_contents('image/'.$imageName, $data);
+			}
+			mysqli_query($konek, "UPDATE login SET pp_name='$imageName' WHERE NIK='$nik' AND Nama_Perusahaan='$kantor'");
+		}
+
+	echo '<img src="image/'.$imageName.'" class="img-thumbnail" />';
+
+}
+
+?>
