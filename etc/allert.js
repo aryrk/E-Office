8104c78b360c7d44a7f9d54ce832d27ff21140c3
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-18 20:34:01 +0700

    Update allert.js

--- etc/allert.js
+++ etc/allert.js
@@ -12,3 +12,33 @@ Toast.fire({
   title: 'Delete successfully'
 })
 };
+
+function update_absen(){
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Delete successfully'
+})
+};
+
+function Login(){
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Signed in successfully'
+})
+};
