024772754bc20ff1a4b432433620201376b53697
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-19 09:55:08 +0700

    Update Data-Absen-Karyawan.php

--- admin/Data-Absen-Karyawan.php
+++ admin/Data-Absen-Karyawan.php
@@ -148,9 +148,9 @@ else if (isset($_GET['l'])){
                 <ul class="inline">
                     <li><a href="Setting-Absen.php" class="absen">Setting Absen<i class="logo fas fa-calendar-check"></i></a></li>
                     <li><a href="Izin-Cuti.html" class="cuti">Izin Cuti<i class="logo fas fa-calendar-minus"></i></a></li>
-                    <li><a href="" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
-                    <li><a href="" class="karyawan">Karyawan<i class="logo fas fa-id-card"></i></a></li>
-                    <li><a href="List-Karyawan.html" class="list-karyawan">List Karyawan<i class="logo fas fa-tasks"></i></a></li>
+                    <li><a href="+Pengumuman.php" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
+                    <li><a href="../Login/regis.php" class="karyawan">Karyawan<i class="logo fas fa-id-card"></i></a></li>
+                    <li><a href="List-Karyawan.php" class="list-karyawan">List Karyawan<i class="logo fas fa-tasks"></i></a></li>
                     <li><a href="" class="tugas">Tugas<i class="logo fas fa-briefcase"></i></a></li>
                 </ul>
             </div>
