f5788a9a564479b0ab6b681a0284271c1523b6f2
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-19 11:35:05 +0700

    Update Tugas.php

--- admin/Tugas.php
+++ admin/Tugas.php
@@ -1,6 +1,5 @@
 <?php
-require_once("../config.php");
-date_default_timezone_set('Asia/Jakarta');
+require_once("../../../config.php");
 session_start();
 //mencegah user masuk bila mereka belum melakukan login
 if (!isset($_SESSION['LOGIN_ADMIN'])){
@@ -12,43 +11,11 @@ $kantor = $_SESSION['kantor_admin'];
 $nik = $_SESSION['NIK_admin'];
 $pw = $_SESSION['PW_admin'];
 
-$tgl = date("Y-m-d");
-$jam = date("H:i:s");
-
-if(isset($_POST['kirim'])){
-	$tujuan = trim($_POST['tujuan']);
-	$tanggal = $_POST["time"];
-	$isi = trim($_POST['isi']);
-		
-		$sql = mysqli_query($konek, "SELECT * FROM data_perusahaan WHERE NIK_Admin='$nik' AND Password='$pw' AND Nama_Perusahaan='$kantor'");
-		
-		if (mysqli_num_rows($sql) != 0){
-			$A = "SELECT * FROM data_perusahaan WHERE NIK_Admin='$nik' AND Password='$pw' AND Nama_Perusahaan='$kantor';";
-			$result = mysqli_query($konek, $A);
-			$check = mysqli_num_rows($result);
-				
-			if ($check > 0){
-				while ($row = mysqli_fetch_assoc($result)){
-					$nama = $row['Nama_Admin'];
-					
-					$sql = mysqli_query($konek, "INSERT INTO tugas VALUES ('$kantor','$nama','$nik','$tanggal','$isi','$tujuan','$jam','$tgl')");
-				}
-//Mengecek apakah tugas berhasil terkirim
-	$sql = mysqli_query($konek, "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' AND Nama_Admin='$nama' AND NIK_Admin='$nik' AND Tanggal='$tanggal' AND Isi_Tugas='$isi' AND Tujuan='$tujuan'");
-		if (mysqli_num_rows($sql) == 0){
-			header("Location: ../etc/error/index.php?condition=12 && kantor=$kantor && nik=$nik && password=$pw");
-		}
-				
-			}
-		}
-	}
-if(isset($_POST['prev'])){
-	$tujuan = trim($_POST['tujuan']);
-	$tanggal = $_POST["time"];
-	$isi = trim($_POST['isi']);
-		$_SESSION['isi'] = $isi;
-	header("Location: preview_tugas/preview.php?tujuan=$tujuan && tanggal=$tanggal");
-	}
+if(isset($_POST['search'])){
+	$nama_cek = trim($_POST['nama']);
+	$nama = date('Y-m-d',strtotime($nama_cek));
+	header("Location: history.php?l=$nama");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -56,95 +23,122 @@ if(isset($_POST['prev'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel = "icon" href ="../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
-    <title>Upload Tugas</title>
+	<link rel = "icon" href ="../../../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
+    <title>Log Pengiriman Tugas</title>
     
-    <link rel="stylesheet" href="etc/style.css">
+    <link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="../etc/wmRemover.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@1,600&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="../Main Tab/etc/Animate.css">
-	
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.all.min.js"></script>
+	<link rel="stylesheet" href="../../../Main Tab/etc/Animate.css">
 </head>
 	
 <body>
     <header class="banner"> 
-        <h1 class="h1"><?php echo $kantor ?> Administrator</h1>
+        <h1 class="h1"><?php echo $kantor; ?> Administrator</h1>
 
-        <a href="Admin.html"><i class="back fas fa-arrow-circle-left"></i></a>
+        <a href="../../Tugas.php"><i class="back fas fa-arrow-circle-left"></i></a>
     </header>
 	<header class="banner-unused"> 
         <h1 class="h1">Officia Administrator</h1>
     </header>
 	
+<!-- partial:index.partial.html -->
+<section class="tampilan">
+    <h2 class="h2">Data Pengiriman Tugas</h2>
 <form id="form1" name="form1" method="post" action="">
-	<section class="tugas_box wow fadeInDown">
-		<center>
-			<h1 class="judul wow zoomIn">Tambah Tugas</h1>
-				<hr class="wow jackInTheBox">
-		</center>
-	<div class="inner wow fadeIn">
-		<label for="isi" class="wow slideInLeft">Tugas Berisi:</label><br>
-		<textarea name="isi" id="isi" rows="2" placeholder="Isi tugas (Styling with Markdown is supported)" required class="isi_tugas"></textarea>
-		
-		<label for="tujuan" class="wow slideInLeft">Akan Dikirimkan Kepada</label>
- 		<select name="tujuan" id="tujuan" class="tujuan wow slideInUp">
-			<option value="Seluruh Karyawan">Seluruh Karyawan</option>
+    <div class="wrap" style="z-index: 10000;">
+		<label for="nama">Cari Berdasarkan Tanggal Pengiriman</label>
+        <div class="search">
+        <input type="date" name="nama" id="nama" class="searchTerm" placeholder="Cari Nama Lengkap..." autocomplete="off">
+        <button type="submit" name="search" id="search" class="searchButton">
+            <i class="fa fa-search"></i>
+        </button>
+        </div>
+    </div>
+	</form>
+    <div class="table-wrapper">
+        <table class="fl-table" border="2" rules="cols">
+            <thead>
+            <tr>
+                <th>Tanggal Pengiriman</th>
+                <th>Isi Tugas</th>
+                <th>Tujuan</th>
+                <th>Tanggal Tujuan</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
 <?php
-	$sql = mysqli_query($konek, "SELECT Jabatan FROM login WHERE Nama_Perusahaan='$kantor'");
+	$sql = mysqli_query($konek, "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor'");
+		
+				
+if (!isset($_GET['l']) || $_GET['l'] == NULL){
 		if (mysqli_num_rows($sql) != 0){
-			$A = "SELECT Jabatan FROM login WHERE Nama_Perusahaan='$kantor' ORDER BY Jabatan DESC;";
+			$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' ORDER BY Tanggal DESC;";
 			$result = mysqli_query($konek, $A);
 			$check = mysqli_num_rows($result);
 				
 			if ($check > 0){
 				while ($row = mysqli_fetch_assoc($result)){
-					$jabatan = $row['Jabatan'];
-			echo '
-				<option value="'.$jabatan.'">'.$jabatan.'</option>
-			';
+					$tanggal_tujuan = $row['Tanggal'];
+					$isi = $row['Isi_Tugas'];
+					$tujuan = $row['Tujuan'];
+					$tanggal = $row['Submitted_On_Date'];
+			echo "<tr><td>" . $tanggal . "</td><td>" . $isi . "</td><td>" . $tujuan . "</td><td>" . $tanggal_tujuan . "</td><td style='padding: 0; background-color: red;'><button style='width: 100%; height: 100%; border: none; color: white; background-color: red; display: block; flex-grow:1;align-items: stretch;'>HAPUS</button></td></tr>";
 		}
 			}
 		}
-			
-	$sql_nama = mysqli_query($konek, "SELECT Nama FROM login WHERE Nama_Perusahaan='$kantor'");
-		if (mysqli_num_rows($sql_nama) != 0){
-			$A = "SELECT Nama FROM login WHERE Nama_Perusahaan='$kantor' ORDER BY Nama DESC;";
+}
+else if (isset($_GET['l'])){
+	$l = $_GET['l'];
+		if (mysqli_num_rows($sql) != 0){
+			$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' AND Submitted_On_Date='$l' ORDER BY Tujuan DESC;";
 			$result = mysqli_query($konek, $A);
 			$check = mysqli_num_rows($result);
 				
 			if ($check > 0){
 				while ($row = mysqli_fetch_assoc($result)){
-					$nama = $row['Nama'];
-			echo '
-				<option value="'.$nama.'">'.$nama.'</option>
-			';
+					$tanggal_tujuan = $row['Tanggal'];
+					$isi = $row['Isi_Tugas'];
+					$tujuan = $row['Tujuan'];
+					$tanggal = $row['Submitted_On_Date'];
+			echo "<tr><td>" . $tanggal . "</td><td>" . $isi . "</td><td>" . $tujuan . "</td><td>" . $tanggal_tujuan . "</td><td style='padding: 0; background-color: red;'><button style='width: 100%; height: 100%; border: none; color: white; background-color: red; display: block; flex-grow:1;align-items: stretch;'>HAPUS</button></td></tr>";
 		}
 			}
 		}
+}
 ?>
-		</select>
-		
-		<label for="time" class="wow slideInLeft"> Pada Tanggal</label>
-		<input class="tujuan wow slideInUp" type="date" name="time" id="time" min="<?php echo $tgl ?>" value="<?php echo $tgl ?>">
-		
-		<center>
-			<button type="submit" class="sumbit wow bounce" value="Kirim" id="kirim" name="kirim">Kirim</button>&nbsp;&nbsp;&nbsp;<button type="submit" class="sumbit wow bounce" value="Preview" id="prev" name="prev">Preview</button>
-		</center>
-	</div>
-	</section>
-</form>
-	
-<div class="menuFloat wow slideInRight" id="Float">
-<table>
-	<tr>
-		<th><p class="menu"><a href="etc/history/history.html">Histori</a></p></th>
-	</tr>	
-</table>
-</div>
+            </tbody>
+        </table>
+    </div>
+</section>
+<!-- partial -->
     
     <div class="Banner-handap">
+        <div class="o">Officia</div>
+        <nav class="navbar">
+            <a class="toggler-navbar">
+                <div class="hamburger-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </a>
+            <div class="sidebar">
+                <ul class="inline">
+                    <li><a href="../../Setting-Absen.php" class="absen">Setting Absen<i class="logo fas fa-calendar-check"></i></a></li>
+                    <li><a href="../../Izin-Cuti.html" class="cuti">Izin Cuti<i class="logo fas fa-calendar-minus"></i></a></li>
+                    <li><a href="../../+Pengumuman.php" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
+                    <li><a href="../../../Login/regis.php" class="karyawan">+Karyawan<i class="logo fas fa-id-card"></i></a></li>
+                    <li><a href="../../List-Karyawan.php" class="list-karyawan">List Karyawan<i class="logo fas fa-tasks"></i></a></li>
+                    <li><a href="../../Tugas.php" class="tugas">Tugas<i class="logo fas fa-briefcase"></i></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <div class="Banner-handap-used">
         <div class="o">Officia</div>
         <nav class="navbar">
             <a class="toggler-navbar">
@@ -167,30 +161,7 @@ if(isset($_POST['prev'])){
         </nav>
     </div>
 
-    <div class="Banner-handap-used">
-        <div class="o">Officia</div>
-        <nav class="navbar">
-            <a class="toggler-navbar">
-                <div class="hamburger-menu">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </a>
-            <div class="sidebar">
-                <ul class="inline">
-                    <li><a href="Setting-Absen.html" class="absen">Setting Absen<i class="logo fas fa-calendar-check"></i></a></li>
-                    <li><a href="Izin-Cuti.html" class="cuti">Izin Cuti<i class="logo fas fa-calendar-minus"></i></a></li>
-                    <li><a href="" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
-                    <li><a href="" class="karyawan">Karyawan<i class="logo fas fa-id-card"></i></a></li>
-                    <li><a href="" class="list-karyawan">List Karyawan<i class="logo fas fa-tasks"></i></a></li>
-                    <li><a href="" class="tugas">Tugas<i class="logo fas fa-briefcase"></i></a></li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-
-    <script src="etc/script.js"></script>
+    <script src="../script.js"></script>
 	<script src="../Main Tab/etc/wow.min.js"></script>
 <script>
 	new WOW().init();
