082ff735d7c2c043e9964d3250b895b835219c61
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-21 15:55:09 +0700

    Update Cuti.php

--- Absensi/Cuti.php
+++ Absensi/Cuti.php
@@ -13,36 +13,29 @@ $kantor = $_SESSION['kantor'];
 $pass = $_SESSION['password'];
 $nama = $_SESSION['nama'];
 
-if(isset($_POST['PROFIL'])){
-	header("Location: ../Main Tab/etc/Main.php");
-}
+date_default_timezone_set('Asia/Jakarta');
+$besok = date("Y-m-d", strtotime( "+1 days" ));
 
-if(isset($_POST['ABSEN'])){
-	header("Location: Absen.php");
-}
-
-if(isset($_POST['DATAABSEN'])){
-	header("Location: DataAbsen.php");
-}
-if(isset($_POST['HOME'])){
-	header("Location: Home.php");
-}
 if(isset($_POST['SUBMIT'])){
 //Mengambil data cuti
-	$jenis = trim($_POST['jenis']);
+	$jenis = trim($_POST['cuti']);
 	$dari = $_POST['dari'];
 	$sampai = $_POST['sampai'];
 	$ket = trim($_POST['ket']);
 	
 	$jam = date("H:i:s");
     $tgl = date("Y-m-d");
+	$id = $nama.time();
 	
-		mysqli_query($konek, "INSERT INTO cuti VALUES ('$nama','$nik','$kantor','$jenis','$dari','$sampai','$ket','$jam','$tgl')");
+		mysqli_query($konek, "INSERT INTO cuti VALUES ('$id','$nama','$nik','$kantor','$jenis','$dari','$sampai','$ket','unknown','$jam','$tgl')");
 					
 		$sql = mysqli_query($konek, "SELECT * FROM cuti WHERE NIK='$nik' AND Nama='$nama' AND Nama_Perusahaan='$kantor' AND Jenis_Cuti='$jenis' AND Dari='$dari' AND Sampai='$sampai' AND Keterangan='$ket' AND Submitted_On_Date='$tgl'");
 			if (mysqli_num_rows($sql) == 0){
 				$_SESSION['condition'] = 14;
 				header("Location: ../etc/error/index.php");
+			}
+			else if (mysqli_num_rows($sql) != 0){
+				header("Location: ../unused.php?value=cuti");
 			}
 	}
 ?>
@@ -52,30 +45,40 @@ if(isset($_POST['SUBMIT'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Cuti</title>
 
     <link rel = "icon" href ="../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
     <link rel="stylesheet" href="styleCuti.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.min.css">
+    <link rel="stylesheet" href="../Main Tab/etc/Animate.css">
 </head>
-<body onLoad="Allert()">
+	
+<?php
+if(isset($_SESSION['first_cuti'])){
+	echo '<body onLoad="Allert()">';
+	unset($_SESSION['first_cuti']);
+}
+else if(!isset($_SESSION['first_cuti'])){
+	echo '<body>';
+}
+	
+?>
     <header>
         <div class="container">
             <div id="logo">
                 <h1><?php echo $kantor; ?></h1>
             </div> 
-            <form id="form1" name="form1" method="post" action=""></form>
                 <nav>
                     <ul>
-                        <li><button style="background-color: transparent; border: none; cursor: pointer;" type="submit" name="HOME" id="HOME" value="home"><a>DASHBOARD</a></li> 
+                        <li><a href="Home.php">DASHBOARD</a></li> 
                         
-                        <li><button style="background-color: transparent; border: none; cursor: pointer;" type="submit" name="ABSEN" id="ABSEN" value="absen"><a>ABSEN</a></button></li>
+                        <li><a href="Absen.php">ABSEN</a></li>
                                 
                         <li><a href="">CUTI</a></li>
                             
-                        <li><button style="background-color: transparent; border: none; cursor: pointer;" type="submit" name="DATAABSEN" id="DATAABSEN" value="absen"><a>DATA ABSEN</a></button></li>
+                        <li><a href="DataAbsen.php">DATA ABSEN</a></li>
                             
-                        <li><button style="background-color: transparent; border: none; cursor: pointer;" type="submit" name="PROFIL" id="PROFIL" value="profil"><a>PROFILE</a></button></li>
+                        <li><a href="../Main Tab/etc/Main.php">PROFILE</a></li>
                     </ul>
                 
                     <div class="menu-toggle">
@@ -85,7 +88,6 @@ if(isset($_POST['SUBMIT'])){
                         <span></span>
                     </div>
                 </nav>
-            </form>
         </div>
     </header>
 
@@ -99,14 +101,14 @@ if(isset($_POST['SUBMIT'])){
                         <td>Jenis Cuti </td>
                         <td> : </td>
                         <td>
-                            <select name="cuti">
-                                <option value=""></option>
-                                <option value="ct">Cuti Tahunan</option>
-                                <option value="cs">Cuti Sakit</option>
-                                <option value="cb">Cuti Besar</option>
-                                <option value="ch">Cuti Hamil</option>
-                                <option value="cp">Cuti Penting</option>
-                                <option value="cbr">Cuti Bersama</option>
+                            <select name="cuti" id="cuti">
+                                <option value="Tidak Tertera"></option>
+                                <option value="Cuti Tahunan">Cuti Tahunan</option>
+                                <option value="Cuti Sakit">Cuti Sakit</option>
+                                <option value="Cuti Besar">Cuti Besar</option>
+                                <option value="Cuti Hamil">Cuti Hamil</option>
+                                <option value="Cuti Penting">Cuti Penting</option>
+                                <option value="Cuti Bersama">Cuti Bersama</option>
                             </select>
                         </td>
                     </tr><br>
@@ -115,7 +117,7 @@ if(isset($_POST['SUBMIT'])){
                         <td>Dari Tanggal</td>
                         <td>:</td>
                         <td>
-                            <input type="date">
+                            <input type="date" name="dari" id="dari" min="<?php echo $besok; ?>" required>
                         </td>
                     </tr>
 
@@ -123,7 +125,7 @@ if(isset($_POST['SUBMIT'])){
                         <td>Sampai Tanggal</td>
                         <td>:</td>
                         <td>
-                            <input type="date">
+                            <input type="date" name="sampai" id="sampai" min="<?php echo $besok; ?>" required>
                         </td>
                     </tr>
 
@@ -131,12 +133,12 @@ if(isset($_POST['SUBMIT'])){
                         <td>Keterangan</td>
                         <td>:</td>
                         <td>
-                            <textarea class="ktr" name="keterangan" cols="30" rows="5"></textarea>
+                            <textarea class="ktr" name="ket" id="ket" cols="30" rows="5"></textarea>
                         </td>
                     </tr>
 
                     <tr>
-                        <td colspan="4" align="center"><button onClick="confirm('Apakah Anda Yakin Ingin Cuti?...')" class="kirim" type="submit" name="kirim">Kirim</button></td> 
+                        <td colspan="4" align="center"><button onClick="confirm('Apakah Anda Yakin Ingin Cuti?...')" class="kirim" type="submit" name="SUBMIT" id="SUBMIT">Kirim</button></td> 
                     </tr>
                 </form>
             </table>
@@ -151,69 +153,44 @@ if(isset($_POST['SUBMIT'])){
                     <th>Dari Tanggal</th>
                     <th>Sampai Tanggal</th>
                     <th>Keterangan</th>
+					<th>Status</th>
+					<th>Diunggah Pada</th>
+					<th>Aksi</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>Content 1</td>
-                    <td>Content 1</td>
-                    <td>Content 1</td>
-                    <td>Content 1</td>
+<?php
+$sql = mysqli_query($konek, "SELECT * FROM cuti WHERE Nama_Perusahaan='$kantor' AND NIK='$nik' AND Nama='$nama'");
+	if (mysqli_num_rows($sql) != 0){
+		$A = "SELECT * FROM cuti WHERE Nama_Perusahaan='$kantor' AND NIK='$nik' AND Nama='$nama' ORDER BY Submitted_On_Date DESC;";
+		$result = mysqli_query($konek, $A);
+		$check = mysqli_num_rows($result);
+				
+		if ($check > 0){
+			while ($row = mysqli_fetch_assoc($result)){
+				$id = $row['id'];
+				$jenis = $row['Jenis_Cuti'];
+				$dari = $row['Dari'];
+				$sampai = $row['Sampai'];
+				$ket = $row['Keterangan'];
+				$stat = $row['Status'];
+				$upload = $row['Submitted_On_Date'];
+				
+				echo '
+				<tr>
+                    <td>'.$jenis.'</td>
+                    <td>'.$dari.'</td>
+                    <td>'.$sampai.'</td>
+                    <td>'.$ket.'</td>
+					<td>'.$stat.'</td>
+					<td>'.$upload.'</td>
+					<td style="padding: 0; background-color: firebrick;"><button style="border: none; color: white; background-color: firebrick; width: 100%;" onclick="window.location.href='."'../unused.php?value=hapusCuti&&idCuti=".$id."'".';">Hapus</button></td>
                 </tr>
-                <tr>
-                    <td>Content 2</td>
-                    <td>Content 2</td>
-                    <td>Content 2</td>
-                    <td>Content 2</td>
-                </tr>
-                <tr>
-                    <td>Content 3</td>
-                    <td>Content 3</td>
-                    <td>Content 3</td>
-                    <td>Content 3</td>
-                </tr>
-                <tr>
-                    <td>Content 4</td>
-                    <td>Content 4</td>
-                    <td>Content 4</td>
-                    <td>Content 4</td>
-                </tr>
-                <tr>
-                    <td>Content 5</td>
-                    <td>Content 5</td>
-                    <td>Content 5</td>
-                    <td>Content 5</td>
-                </tr>
-                <tr>
-                    <td>Content 6</td>
-                    <td>Content 6</td>
-                    <td>Content 6</td>
-                    <td>Content 6</td>
-                </tr>
-                <tr>
-                    <td>Content 7</td>
-                    <td>Content 7</td>
-                    <td>Content 7</td>
-                    <td>Content 7</td>
-                </tr>
-                <tr>
-                    <td>Content 8</td>
-                    <td>Content 8</td>
-                    <td>Content 8</td>
-                    <td>Content 8</td>
-                </tr>
-                <tr>
-                    <td>Content 9</td>
-                    <td>Content 9</td>
-                    <td>Content 9</td>
-                    <td>Content 9</td>
-                </tr>
-                <tr>
-                    <td>Content 10</td>
-                    <td>Content 10</td>
-                    <td>Content 10</td>
-                    <td>Content 10</td>
-                </tr>
+				';
+			}
+		}
+	}
+?>
                 </tbody>
             </table>
         </div>
@@ -228,6 +205,18 @@ if(isset($_POST['SUBMIT'])){
     </footer>
 
     <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.all.min.js"></script>
+	<script src="../etc/allert.js"></script>
+	
+	<?php
+		if (isset($_SESSION['cuti'])){
+			echo "<script> cuti(); </script>";
+			unset($_SESSION['cuti']);
+		}
+		if (isset($_SESSION['hapusCuti'])){
+			echo "<script> hapusCuti(); </script>";
+			unset($_SESSION['hapusCuti']);
+		}
+?>
 </body>
 </html>
