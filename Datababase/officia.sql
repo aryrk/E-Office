b97afc5d9f7656c2a24e29c747147900a61f707d
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-22 18:54:20 +0700

    Update officia.sql

--- Datababase/officia.sql
+++ Datababase/officia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2021 at 11:02 AM
+-- Generation Time: Mar 22, 2021 at 12:53 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
