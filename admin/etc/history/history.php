2d18cc71f0c052cbd244307f2cc8a41401be2c7a
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-22 10:19:27 +0700

    Update history.php

--- admin/etc/history/history.php
+++ admin/etc/history/history.php
@@ -3,18 +3,28 @@ require_once("../../../config.php");
 session_start();
 //mencegah user masuk bila mereka belum melakukan login
 if (!isset($_SESSION['LOGIN_ADMIN'])){
-	header("Location: ../Login/Loginadmin.php");
+	header("Location: ../../../Login/Loginadmin.php");
 	exit ();
 }
+
+date_default_timezone_set('Asia/Jakarta');
+$tgl = date("Y-m-d");
 
 $kantor = $_SESSION['kantor_admin'];
 $nik = $_SESSION['NIK_admin'];
 $pw = $_SESSION['PW_admin'];
 
 if(isset($_POST['search'])){
-	$nama_cek = trim($_POST['nama']);
-	$nama = date('Y-m-d',strtotime($nama_cek));
-	header("Location: history.php?l=$nama");
+	$jenis = trim($_POST['jenis']);
+	header("Location: history.php?l=$jenis");
+}
+if(isset($_POST['search_2'])){
+	$date = trim($_POST['date']);
+	$date = date('Y-m-d',strtotime($date));
+	header("Location: history.php?l=$date&&ldate=$date");
+}
+if(isset($_POST['reset'])){
+	header("Location: history.php");
 }
 ?>
 <!DOCTYPE html>
@@ -35,107 +45,207 @@ if(isset($_POST['search'])){
 	
 <body>
     <header class="banner"> 
-        <h1 class="h1"><?php echo $kantor; ?> Administrator</h1>
+        <h1 class="h1">Officia Administrator</h1>
 
         <a href="../../Tugas.php"><i class="back fas fa-arrow-circle-left"></i></a>
     </header>
 	<header class="banner-unused"> 
         <h1 class="h1">Officia Administrator</h1>
     </header>
-	
-<!-- partial:index.partial.html -->
-<section class="tampilan">
-    <h2 class="h2">Data Pengiriman Tugas</h2>
+<!--Inner-->
 <form id="form1" name="form1" method="post" action="">
-    <div class="wrap" style="z-index: 10000;">
-		<label for="nama">Cari Berdasarkan Tanggal Pengiriman</label>
-        <div class="search">
-        <input type="date" name="nama" id="nama" class="searchTerm" autocomplete="off">
-        <button type="submit" name="search" id="search" class="searchButton">
-            <i class="fa fa-search"></i>
-        </button>
-        </div>
-    </div>
-	</form>
-    <div class="table-wrapper">
-        <table class="fl-table" border="2" rules="cols">
-            <thead>
-            <tr>
-                <th>Tanggal Pengiriman</th>
-                <th>Isi Tugas</th>
-                <th>Tujuan</th>
-                <th>Tanggal Tujuan</th>
-                <th>Action</th>
-            </tr>
-            </thead>
-            <tbody>
+	<div class="wrap" style="z-index: 10000;">
+	<div class="search">
+		<select name="jenis" id="jenis">
 <?php
-	$sql = mysqli_query($konek, "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor'");
+			if (!isset($_GET['l']) && !isset($_GET['ldate'])){
+			echo '
+				<option value="All" class="searchTerm">Semua Tugas Terkirim</option>
+				<option value="Global" class="searchTerm">Tugas Global</option>
+				';
+			}
+			else if (isset($_GET['l']) && !isset($_GET['ldate'])){
+				$cari = $_GET['l'];
+			echo '
+				<option value="'.$cari.'" class="searchTerm">Tugas '.$cari.'</option>
+				<option value="All" class="searchTerm">Semua Tugas Terkirim</option>
+				<option value="Global" class="searchTerm">Tugas Global</option>
+				';
+			}
+			else {
+			echo '
+				<option value="All" class="searchTerm">Semua Tugas Terkirim</option>
+				<option value="Global" class="searchTerm">Tugas Global</option>
+				';
+			}
+
+	$sql = mysqli_query($konek, "SELECT Jabatan FROM login WHERE Nama_Perusahaan='$kantor'");
+		if (mysqli_num_rows($sql) != 0){
+			$A = "SELECT Jabatan FROM login WHERE Nama_Perusahaan='$kantor' ORDER BY Jabatan DESC;";
+			$result = mysqli_query($konek, $A);
+			$check = mysqli_num_rows($result);
+				
+			if ($check > 0){
+				while ($row = mysqli_fetch_assoc($result)){
+					$jabatan = $row['Jabatan'];
+			echo '
+				<option value="'.$jabatan.'">Tugas Bagian '.$jabatan.'</option>
+			';
+		}
+			}
+		}
+			
+	$sql_nama = mysqli_query($konek, "SELECT Nama FROM login WHERE Nama_Perusahaan='$kantor'");
+		if (mysqli_num_rows($sql_nama) != 0){
+			$A = "SELECT Nama FROM login WHERE Nama_Perusahaan='$kantor' ORDER BY Nama DESC;";
+			$result = mysqli_query($konek, $A);
+			$check = mysqli_num_rows($result);
+				
+			if ($check > 0){
+				while ($row = mysqli_fetch_assoc($result)){
+					$nama = $row['Nama'];
+			echo '
+				<option value="'.$nama.'">Tugas '.$nama.'</option>
+			';
+		}
+			}
+		}
+
+if(!isset($_GET['ldate'])){
+			echo'
+		</select>
+		<button type="submit" name="search" id="search" class="searchButton">
+			<i class="fa fa-search"></i>
+		</button>
 		
+		<label style="padding: 6px;">Atau</label><input type="date" name="date" id='."date".' value="'.$tgl.'"/>
+		<button type="submit" name="search_2" id="search_2" class="searchButton">
+			<i class="fa fa-search"></i>
+		</button>';
+}
+else if(isset($_GET['ldate'])){
+	$tanggal_cari = $_GET['ldate'];
+			echo'
+		</select>
+		<button type="submit" name="search" id="search" class="searchButton">
+			<i class="fa fa-search"></i>
+		</button>
+		
+		<label style="padding: 6px;">Atau</label><input type="date" name="date" id='."date".' value="'.$tanggal_cari.'"/>
+		<button type="submit" name="search_2" id="search_2" class="searchButton">
+			<i class="fa fa-search"></i>
+		</button>';
+}
+			?>
+	</div>
+		<br><button type="submit" name="reset" id="reset" class="searchButton_2">Reset Pencarian</button>
+		</div>
+	</form>
+	
+<div class="row">
+<?php
+	if (!isset($_GET['l']) || $_GET['l'] == NULL || $_GET['l'] == "All"){
+		$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' ORDER BY Submitted_On_Date DESC;";
+		$result = mysqli_query($konek, $A);
+		$check = mysqli_num_rows($result);
 				
-if (!isset($_GET['l']) || $_GET['l'] == "1970-01-01"){
-		if (mysqli_num_rows($sql) != 0){
-			$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' ORDER BY Tanggal DESC;";
-			$result = mysqli_query($konek, $A);
-			$check = mysqli_num_rows($result);
+		if ($check > 0){
+			while ($row = mysqli_fetch_assoc($result)){
+				$judul = $row['Judul'];
+				$pengiriman = $row['Submitted_On_Date'];
+				$tujuan = $row['Tujuan'];
+				$id = $row['id_tugas'];
 				
-			if ($check > 0){
-				while ($row = mysqli_fetch_assoc($result)){
-					$tanggal_tujuan = $row['Tanggal'];
-					$isi_cek = $row['Isi_Tugas'];
-					$tujuan = $row['Tujuan'];
-					$tanggal = $row['Submitted_On_Date'];
-					$jam = $row['Submitted_On_Hours'];
-					
-					$cek = strlen($isi_cek);
-					if ($cek >= 10){
-						$isi_cek2 = substr($isi_cek, 0, 10);
-						$a = htmlentities($isi_cek2);
-						$isi = $a.'...'. '<a href="../../preview_tugas/preview.php?kantor='.$kantor.'&&nik='.$nik.'&&tanggal='.$tanggal.'&&jam='.$jam.'&&his=1'.'" style="color: blue;"> Selengkapnya...</a>';
-					}
-					else if ($cek < 10) {
-						$isi = $isi_cek;
-					}
-			echo "<tr><td>" . $tanggal . "</td><td>" . $isi . "</td><td>" . $tujuan . "</td><td>" . $tanggal_tujuan . "</td><td style='padding: 0; background-color: red;'><button style='width: 100%; height: 100%; border: none; color: white; background-color: red; display: block; flex-grow:1;align-items: stretch;'>HAPUS</button></td></tr>";
-		}
+				echo '
+  		<div class="column">
+    		<div class="card">
+      			<img src="../../../Icon/Textless/Icon.png" alt="Logo" style="width:100%">
+      			<div class="container">
+        			<h2 class="nama">'.$judul.'</h2>
+		  			<div class="textB">
+        			<p class="title">'.$pengiriman.'</p>
+        			<p style="font-size: 90%">'.$tujuan.'</p>
+					<p>'.$kantor.' Company</p>
+			  		</div>
+        			<p><a href="../../../unused.php?value=prevtugas&&id_tugas='.$id.'"><button class="button" id="button1">Lihat Tugas</button></a></p>
+      			</div>
+				<p><a href="../../../unused.php?value=deltugas&&id_tugas='.$id.'"><button class="button_hapus" id="button_hapus">HAPUS</button></a></p>
+    		</div>
+  		</div>
+		';
 			}
 		}
-}
-else if (isset($_GET['l'])){
-	$l = $_GET['l'];
-		if (mysqli_num_rows($sql) != 0){
-			$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' AND Submitted_On_Date='$l' ORDER BY Tanggal DESC;";
-			$result = mysqli_query($konek, $A);
-			$check = mysqli_num_rows($result);
+	}
+	
+	else if (isset($_GET['l'])){
+		$l = $_GET['l'];
+		$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' AND Tujuan='$l' OR Nama_Perusahaan='$kantor' AND Submitted_On_Date='$l' ORDER BY Submitted_On_Date DESC;";
+		$result = mysqli_query($konek, $A);
+		$check = mysqli_num_rows($result);
 				
-			if ($check > 0){
-				while ($row = mysqli_fetch_assoc($result)){
-					$tanggal_tujuan = $row['Tanggal'];
-					$isi_cek = $row['Isi_Tugas'];
-					$tujuan = $row['Tujuan'];
-					$tanggal = $row['Submitted_On_Date'];
-					$jam = $row['Submitted_On_Hours'];
-					
-					$cek = strlen($isi_cek);
-					if ($cek >= 10){
-						$isi_cek2 = substr($isi_cek, 0, 10);
-						$a = htmlentities($isi_cek2);
-						$isi = $a.'...'. '<a href="../../preview_tugas/preview.php?kantor='.$kantor.'&&nik='.$nik.'&&tanggal='.$tanggal.'&&jam='.$jam.'&&his=1'.'" style="color: blue;"> Selengkapnya...</a>';
-					}
-					else if ($cek < 10) {
-						$isi = $isi_cek;
-					}
-			echo "<tr><td>" . $tanggal . "</td><td>" . $isi . "</td><td>" . $tujuan . "</td><td>" . $tanggal_tujuan . "</td><td style='padding: 0; background-color: red;'><button style='width: 100%; height: 100%; border: none; color: white; background-color: red; display: block; flex-grow:1;align-items: stretch;'>HAPUS</button></td></tr>";
-		}
+		if ($check > 0){
+			while ($row = mysqli_fetch_assoc($result)){
+				$judul = $row['Judul'];
+				$pengiriman = $row['Submitted_On_Date'];
+				$tujuan = $row['Tujuan'];
+				$id = $row['id_tugas'];
+				
+				echo '
+  		<div class="column">
+    		<div class="card">
+      			<img src="../../../Icon/Textless/Icon.png" alt="Logo" style="width:100%">
+      			<div class="container">
+        			<h2 class="nama">'.$judul.'</h2>
+		  			<div class="textB">
+        			<p class="title">'.$pengiriman.'</p>
+        			<p style="font-size: 90%">'.$tujuan.'</p>
+					<p>'.$kantor.' Company</p>
+			  		</div>
+        			<p><a href="../../../unused.php?value=prevtugas&&id_tugas='.$id.'"><button class="button" id="button1">Lihat Tugas</button></a></p>
+      			</div>
+				<p><a href="../../../unused.php?value=deltugas&&id_tugas='.$id.'"><button class="button_hapus" id="button_hapus">HAPUS</button></a></p>
+    		</div>
+  		</div>
+		';
 			}
 		}
-}
+	}
+	
+	if (isset($_GET['l']) == "Global"){
+		$A = "SELECT * FROM tugas WHERE Nama_Perusahaan='$kantor' AND Tujuan='Seluruh Karyawan' ORDER BY Submitted_On_Date DESC;";
+		$result = mysqli_query($konek, $A);
+		$check = mysqli_num_rows($result);
+				
+		if ($check > 0){
+			while ($row = mysqli_fetch_assoc($result)){
+				$judul = $row['Judul'];
+				$pengiriman = $row['Submitted_On_Date'];
+				$tujuan = $row['Tujuan'];
+				$id = $row['id_tugas'];
+				
+				echo '
+  		<div class="column">
+    		<div class="card">
+      			<img src="../../../Icon/Textless/Icon.png" alt="Logo" style="width:100%">
+      			<div class="container">
+        			<h2 class="nama">'.$judul.'</h2>
+		  			<div class="textB">
+        			<p class="title">'.$pengiriman.'</p>
+        			<p style="font-size: 90%">'.$tujuan.'</p>
+					<p>'.$kantor.' Company</p>
+			  		</div>
+        			<p><a href="../../../unused.php?value=prevtugas&&id_tugas='.$id.'"><button class="button" id="button1">Lihat Tugas</button></a></p>
+      			</div>
+				<p><a href="../../../unused.php?value=deltugas&&id_tugas='.$id.'"><button class="button_hapus" id="button_hapus">HAPUS</button></a></p>
+    		</div>
+  		</div>
+		';
+			}
+		}
+	}
 ?>
-            </tbody>
-        </table>
-    </div>
-</section>
-<!-- partial -->
+</div>
+<!--Inner-->
     
     <div class="Banner-handap">
         <div class="o">Officia</div>
@@ -183,8 +293,8 @@ else if (isset($_GET['l'])){
         </nav>
     </div>
 
-    <script src="../script.js"></script>
-	<script src="../Main Tab/etc/wow.min.js"></script>
+    <script src="../../script.js"></script>
+	<script src="../../../Main Tab/etc/wow.min.js"></script>
 <script>
 	new WOW().init();
 </script>
