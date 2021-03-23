5721d6aac4b708d316165b50625b3b4825010f52
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-23 09:42:41 +0700

    Update Setting-Absen.php

--- admin/Setting-Absen.php
+++ admin/Setting-Absen.php
@@ -105,6 +105,7 @@ if(isset($_POST['ABSEN_PULANG'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel = "icon" href ="../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
     <title>Setting Absen</title>
 
     <link rel="stylesheet" href="Setting-Absen.css">
