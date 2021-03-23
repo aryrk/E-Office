cbd1a456357439755d99394e2c7835e4a3fab90c
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-23 10:36:55 +0700

    Update Tugas.php

--- admin/Tugas.php
+++ admin/Tugas.php
@@ -12,6 +12,14 @@ $kantor = $_SESSION['kantor_admin'];
 $nik = $_SESSION['NIK_admin'];
 $pw = $_SESSION['PW_admin'];
 
+$count_nama_kantor = strlen($kantor);
+if ($count_nama_kantor <= 7){
+	$nama_kantor = $kantor." Administrator";
+}
+else {
+	$nama_kantor = $kantor;
+}
+
 $tgl = date("Y-m-d");
 $jam = date("H:i:s");
 
@@ -75,7 +83,7 @@ if(isset($_POST['prev'])){
 	
 <body>
     <header class="banner"> 
-        <h1 class="h1"><?php echo $kantor ?> Administrator</h1>
+        <h1 class="h1"><?php echo $nama_kantor; ?></h1>
 
         <a href="Admin1.php"><i class="back fas fa-arrow-circle-left"></i></a>
     </header>
