af185bbdc15ce2aa0b2d7dcf3a8778ea44a7589f
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-23 09:41:26 +0700

    Update Data-Absen-Karyawan.php

--- admin/Data-Absen-Karyawan.php
+++ admin/Data-Absen-Karyawan.php
@@ -22,7 +22,7 @@ if(isset($_POST['search'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel = "icon" href ="../../../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
+	<link rel = "icon" href ="../Icon/Sign_only_Inverted/Transparent.png" type = "image/x-icon">
     <title>Log Tugas</title>
     
     <link rel="stylesheet" href="Data-Absen-Karyawan.css">
