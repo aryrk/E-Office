55485d4a60e836dcb70c3c5ae788542d904c69a6
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-25 11:23:21 +0700

    Update History-Izin-Cuti.php

--- admin/History-Izin-Cuti.php
+++ admin/History-Izin-Cuti.php
@@ -228,7 +228,7 @@ else if (isset($_GET['l'])){
                 <ul class="inline">
                     <li><a href="Setting-Absen.php" class="absen">Setting Absen<i class="logo fas fa-calendar-check"></i></a></li>
                     <li><a href="Izin-Cuti.php" class="cuti">Izin Cuti<i class="logo fas fa-calendar-minus"></i></a></li>
-                    <li><a href="" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
+                    <li><a href="+Pengumuman.php" class="pengumuman">Pengumuman<i class="logo fas fa-bullhorn"></i> </a></li>
                     <li><a href="../Login/regis.php" class="karyawan">+Karyawan<i class="logo fas fa-id-card"></i></a></li>
                     <li><a href="List-Karyawan.php" class="list-karyawan">List Karyawan<i class="logo fas fa-tasks"></i></a></li>
                     <li><a href="Tugas.php" class="tugas">Tugas<i class="logo fas fa-briefcase"></i></a></li>
