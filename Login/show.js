324c675e240e0e84bcdb5e3113576a37edb12d89
Juragan Ayam Kampung <78460636+Ikiayam@users.noreply.github.com>
2021-03-26 19:42:51 +0700

    Add files via upload

---
+++ Login/show.js
@@ -0,0 +1,14 @@
+const visibilityToogle = document.querySelector('.visibility');
+const input = document.querySelector('.form-contener input');
+var password = true;
+visibilityToogle.addEventListener('click', function() {
+if (password) {
+input.setAttribute('type', 'text');
+visibilityToogle.innerHTML = 'visibility';
+}
+else {
+input.setAttribute('type', 'password');
+visibilityToogle.innerHTML = 'visibility_off';
+}
+password = !password;
+});
