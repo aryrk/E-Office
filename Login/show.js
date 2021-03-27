ce503640611c5ffa6b56e2716dcfdd3f5111d3fd
Juragan Ayam Kampung <78460636+Ikiayam@users.noreply.github.com>
2021-03-27 18:29:00 +0700

    Update show.js

--- Login/show.js
+++ Login/show.js
@@ -1,14 +1,22 @@
 const visibilityToogle = document.querySelector('.visibility');
 const input = document.querySelector('.form-contener input');
+const input1 = document.querySelector('.fai input');
+
 var password = true;
+
 visibilityToogle.addEventListener('click', function() {
 if (password) {
 input.setAttribute('type', 'text');
+input1.setAttribute('type', 'text');
 visibilityToogle.innerHTML = 'visibility';
 }
 else {
 input.setAttribute('type', 'password');
+input1.setAttribute('type', 'password');
 visibilityToogle.innerHTML = 'visibility_off';
 }
 password = !password;
 });
+
+//repeat 
+
