b79b742473f9c385c332ccb96cc7f716df48ce2f
Tragedy <amanwhohave@gmail.com>
2021-04-04 11:47:39 +0700

    Update allert.js

--- etc/allert.js
+++ etc/allert.js
@@ -160,3 +160,18 @@ function Logout(){
 		}
 	})
 };
+
+function kirimTugas(){
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Upload assignment successfully'
+})
+};
