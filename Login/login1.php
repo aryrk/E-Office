f575ea8056d36c6edc6126ce74f6c25eba8a2282
Juragan Ayam Kampung <78460636+Ikiayam@users.noreply.github.com>
2021-04-30 08:29:38 +0700

    Update login1.php

--- Login/login1.php
+++ Login/login1.php
@@ -168,7 +168,7 @@ jQuery(function($) {
     });
 });
 </script>
-<script src="show.js"></script>
+<script src="falsepass.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.all.min.js"></script>
 	<script src="../etc/allert.js"></script>
 	
